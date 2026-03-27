1095b9dc76fb9116eb1c9b44a66b4e63bb928b21
Leonardo Valle <lvalle@2mundos.net>
2026-03-26 21:13:04 -0300

    visual adjusts on pricing

--- src/Plans/Plans.ts
+++ src/Plans/Plans.ts
@@ -1,7 +1,11 @@
 export const plans = [
   {
     name: 'Pro',
-    price: 'USD 5',
-    features: ['5 aulas por dia', 'Acesso à plataforma', 'Suporte prioritário'],
+    price: '5',
+    features: [
+      'Treinos ilimitados',
+      'Acesso mais 5 mil perguntas desenvolvidas por especialistas',
+      'Suporte prioritário',
+    ],
   },
 ];
